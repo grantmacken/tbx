e1bcc8d6d83db09eb582a5e002b3b77c1564e19a
grantmacken <grantmacken@gmail.com>
2025-12-01 09:25:17 +1300

    wip:7 dnf to tr

--- inc/define.mk
+++ inc/define.mk
@@ -35,3 +35,12 @@ define dnf_installed_info
 	# consistent write to file format
 	$(call to_info,$(1),$${VER},$${SUM})
 endef
+
+define dnf_to_table_row
+  	LINES=$$($(RUN) dnf info --installed $(1))
+	# extract 'name', 'version', 'summary'
+	VER=$$(echo "$${LINES}" | grep -oP '^Version\s+:\s+\K.+' || true)
+	SUM=$$(echo "$${LINES}" | grep -oP '^Summary\s+:\s+\K.+' || true)
+	# consistent write to file format
+	$(call tr,$(1),$${VER},$${SUM},$(2))
+endef
